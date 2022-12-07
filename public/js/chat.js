eaee9063f831cfbeb96c8a8de6718b91986e4bb0
ramdevsharma-kiwi <ram.sharma@kiwitech.com>
2022-12-07 18:54:59 +0530

    this is chat

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,21 +1,19 @@
 const socket = io()
-socket.on('message',(message)=>{
+socket.on('message',(message) =>{
     console.log(message)
 })
-
 document.querySelector('#message-form').addEventListener('submit',(e)=>{
     e.preventDefault()
+    const message = e.target.elements.message.value
 
-    const message = document.querySelector('input').value
-        
-    socket.emit('sendMessage',message)
+    socket.emit('sendMessage', message)
+}) 
+
+document.querySelector('#send-location').addEventListener('click',() =>  {
+    if(!navigator.geolocation){
+        return alert(`Geolocation is not supported by your browsere..`)
+    }
+    navigator.geolocation.getCurrentPosition((position)=>{
+        console.log(position)
+    })
 })
-
-// socket.on('countUpdated',(count)=>{
-//     console.log(`the count has been updated!`,count)
-// }) 
-
-// document.querySelector('#increment').addEventListener('click',()=>{
-//     console.log('clicked')
-//     socket.emit('increment')
-// })
--- src/index.js
+++ src/index.js
@@ -12,23 +12,18 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-// let count =0
-
 io.on('connection',(socket) =>{
     console.log(`new websocket connection`)
 
     socket.emit('message','welcome!')
+    socket.broadcast.emit('message','A new user has joined!..')
 
-    socket.on('sendMesage', (message)=>{
+    socket.on('sendMessage',(message) => {
         io.emit('message',message)
     })
-    // socket.emit('countUpdated',count)
-
-    // socket.on("increment", ()=>{
-    //     count++
-    //     // socket.emit('countUpdated', count)
-    //     io.emit('countUpdated')
-    // })
+    socket.on('disconnect',()=>{
+        io.emit('message', 'A user has left!..')
+    })
 })
 
 
